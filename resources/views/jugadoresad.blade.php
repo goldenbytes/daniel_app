720c04ce79295ed642d1da42218f2efe6a22f45e
SebastiaN <odm_sd@hotmail.com>
2017-02-23 21:15:21 -0500

    rutas

--- resources/views/jugadoresad.blade.php
+++ resources/views/jugadoresad.blade.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="UTF-8">
     <title>Pirate Party</title>
-    <link rel="stylesheet" href="css/materialize.min.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/MyCSS.css">
+    <link rel="stylesheet" href="{!! asset('css/materialize.min.css')!!}">
+    <link rel="stylesheet" href="{!! asset('css/font-awesome.min.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/MyCSS.css') !!}">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
     <link rel="icon" type="image/x-icon" href="http://icons.iconarchive.com/icons/aha-soft/torrent/512/pirate-icon.png" />
   </head>
@@ -53,11 +53,7 @@
           </div>
   		</div>
   	</div>
-
-
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script src="js/materialize.min.js"></script>
-    </script>
-    </script>
+    <script type="text/javascript" src="{!! asset('js/jquery.min.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('js/materialize.min.js') !!}"></script>
   </body>
 </html>
--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html >
 <head>
-  <meta charset="UTF-8">
-  <title>Pirate Party</title>
-  <link rel="stylesheet" href="css/materialize.min.css">
-  <link rel="stylesheet" href="css/font-awesome.min.css">
-  <link rel="stylesheet" href="css/MyCSS.css">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
-  <link rel="icon" type="image/x-icon" href="http://icons.iconarchive.com/icons/aha-soft/torrent/512/pirate-icon.png" />
+    <meta charset="UTF-8">
+    <title>Pirate Party</title>
+    <link rel="stylesheet" href="{!! asset('css/materialize.min.css')!!}">
+    <link rel="stylesheet" href="{!! asset('css/font-awesome.min.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/MyCSS.css') !!}">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="http://icons.iconarchive.com/icons/aha-soft/torrent/512/pirate-icon.png" />
 </head>
 <body>
   <div class="fondo2">
@@ -26,7 +26,7 @@
       </div>
 		</div>
 	</div>
-  <script type="text/javascript" src="js/jquery.min.js"></script>
-  <script src="js/materialize.min.js"></script>
+  <script type="text/javascript" src="{!! asset('js/jquery.min.js') !!}"></script>
+  <script type="text/javascript" src="{!! asset('js/materialize.min.js') !!}"></script>
 </body>
 </html>
